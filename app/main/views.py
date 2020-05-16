3a176214de5eedbe0bbacc6a6047effbc472bd42
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2020-05-16 13:20:28 +0700

    add error page

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, render_template
-
+from flask_login import (
+    current_user,
+    login_required
+)
 from app.models import EditableHTML
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
+@login_required
 def index():
     return render_template('main/index.html')
 
