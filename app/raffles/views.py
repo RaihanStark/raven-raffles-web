70223f6603bd4f9e3b9c573d8eb10667d1057138
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2020-06-08 16:40:07 +0700

    add api endpoint for products

--- app/raffles/views.py
+++ app/raffles/views.py
@@ -8,10 +8,14 @@ from flask import (
     url_for,
 )
 from flask_login import current_user, login_required
-
+from app.models import Product, Task
 
 raffles = Blueprint('raffles', __name__)
 
-@raffles.route('/')
-def index():
-    return {'msg':'success'}
+@raffles.route('/products/<int:id>', methods=['GET'])
+def index(id):
+    try:
+        selected_product = Product.query.filter_by(id=id).first()
+        return {'id':selected_product.id,'name':selected_product.name,'size':selected_product.sizes}
+    except:
+        return {'error': True, 'msg':'Product not found'},404
