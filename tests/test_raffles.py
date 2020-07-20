b90e057a0845ee49bb89e503ee7d520b100f5c19
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-07-20 22:39:51 +0700

    add discord test case

--- tests/test_raffles.py
+++ tests/test_raffles.py
@@ -3,7 +3,12 @@ import unittest
 from app import create_app, db
 from flask import current_app
 from app.models import Role, User, Product, Task, Profile
+from app.utils import send_to_webhooks
 import json
+
+from config import Config
+
+
 class RafflesTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app('testing')
@@ -22,22 +27,30 @@ class RafflesTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_get_product_by_id(self):
-        res = self.app_test.get('/raffles/products/1',content_type='application/json')
+        res = self.app_test.get('/raffles/products/1',
+                                content_type='application/json')
         data = json.loads(res.get_data())
         self.assertEqual(1, data['id'])
 
     def test_get_product_by_id_error(self):
-        res = self.app_test.get('/raffles/products/999',content_type='application/json')
+        res = self.app_test.get('/raffles/products/999',
+                                content_type='application/json')
         data = json.loads(res.get_data())
         self.assertEqual(True, data['error'])
-        
+
     def test_get_variant_product(self):
         p = Product(name="Adidas - Yeezy Boost 700")
         self.assertEqual(p.get_variant(), "Yeezy Boost 700")
-        
 
-    
-
-        
-        
-
+    def test_send_to_webhook(self):
+        data = {
+            'name': 'Yeezy Boost',
+            'size': 'US 10',
+            'profiles': 'Private Account',
+            'proxies': 'USA',
+            'entries': 10,
+            'status': 'Success'
+        }
+        r = send_to_webhooks(
+            Config.WEBHOOK_TEST_URL, data)
+        self.assertTrue(r)
